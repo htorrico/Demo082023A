cd9dd8525843916380b08541d87e70a1ff7eb1d9
htorricotecsup <53871912+htorrico@users.noreply.github.com>
2023-05-10 10:42:34 -0500

    Add project files.

---
+++ Demo082023A/Demo082023A/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo082023A
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new Page1();
+            MainPage = new Page2();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
